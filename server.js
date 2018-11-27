27f574ff462cfdde412f4f2dc0e45bb3313e8cd4
Aktan <thekambipg@gmail.com>
2018-11-27 01:36:28 -1000

    Horku-postbuild

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const path = require('path');
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use(session({
-    secret: 'SECRET',
+    secret: 'SECRET', // key 
     resave: false,
     saveUninitialized: true
 }));
@@ -26,5 +27,12 @@ app.use('/api/auth/signup', require('./api/auth/signup/index'));
 app.use('/api/auth/login', require('./api/auth/login/index'));
 app.use('/api/auth/', require('./api/auth/additionalRoute'));
 app.use('/api/users', require('./api/users'));
+
+app.use('/static/', express.static(path.join(__dirname, 'client/build/static')));
+app.use('/client/', express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
 app.listen(5000, function() { console.log('__[ Server started ]__') });
